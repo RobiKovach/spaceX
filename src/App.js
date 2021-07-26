ac4946f90c69aae8d4d68e57a6a433489c1ec98e
RobiKovach <36238115+RobiKovach@users.noreply.github.com>
2021-07-26 17:42:02 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './style.css'
+import Header from './components/header/Header'
+import Main from "./components/Main/Main";
+import Features from "./components/features/Features";
+import Footer from "./components/footer/Footer";
+
+
+function App() {
+  return (
+  <>
+  <Header />
+  <Main />
+  <Features />
+  <Footer />
+  	
+  </>
+  );
+}
+
+export default App;
